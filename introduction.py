d344aa7a030fd7f15c2a4a6f3959da0c2e690c9e
codedbashir <codedbashir@gmail.com>
2021-11-04 13:16:49 +0100

    Here is a commit message

---
+++ introduction.py
@@ -0,0 +1,8 @@
+# This is to know the user's name
+first_name = input("Kindly provide your firstname")
+last_name = input("Pls provide your last name")
+print(f'The user fullname is {first_name}  {last_name}') 
+
+# Average age of 6 boys
+age = int(input('Kindly insert the ages'))
+ave_age = age / 6
---
+++ value_of _exp.py
@@ -0,0 +1,2 @@
+value_exp = 100/5
+print(f'The value of the above expression is {value_exp}')
