7baf0b5609be9362bbf27ea422dacadcc47f4eab
codedbashir <codedbashir@gmail.com>
2021-11-04 11:01:22 +0100

    here is a commit message

---
+++ My-Name.py
@@ -0,0 +1,3 @@
+first_name ="Bashir"
+last_name = "Salako"
+print(f'My name is {first_name} {last_name}')
---
+++ Practice.py
@@ -0,0 +1,18 @@
+# No of Siblings
+no_of_siblings = 10
+print(f'I have {no_of_siblings} siblings')
+
+#Task 3: Favourite Food
+favourite_food = "Pounded Yam"
+print(f'My favorite food is {favourite_food}')
+
+num1 = 12
+num2 = 20
+add_num = num1 + num2
+sub_num = num1 - num2
+mult_num = num1 * num2
+div_num = num1 / num2
+flo_div_num = num1 // num2
+modu_num = num1 % num2
+expo_num = num1 ** 2
+print(f'Addition of no is {add_num}, Subtraction is {sub_num}, multiplication is {mult_num}, division is {div_num}, float division is {flo_div_num}, modulus is {modu_num} and exponential is {expo_num}')
