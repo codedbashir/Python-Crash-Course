a1df175f77aeb3088ffbc3e39b8cda5c7af84092
Akeem Lagundoye <38335545+Aifedayo@users.noreply.github.com>
2021-11-05 16:58:52 +0100

    Update area-of-triangle.py

--- Task_1.py/Task-2.py/area-of-triangle.py
+++ Task_1.py/Task-2.py/area-of-triangle.py
@@ -1,5 +1,8 @@
 # User to provide dimension
+
 base_of_triangle = int(input('Please insert the length '))
 height_of_triangle = int(input('Please insert the width '))
+
 area_of_triangle = 0.5 * base_of_triangle * height_of_triangle
+
 print(f'The area of your triangle is {area_of_triangle}')
